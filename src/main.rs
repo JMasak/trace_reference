d013633f560a8101423a144b8a8267d9aaaec358
Jan Masak <Jan.Masak@sbdinc.com>
2025-06-26 07:53:33 +0200

    added function setup_trace

--- src/main.rs
+++ src/main.rs
@@ -31,22 +31,43 @@ async fn handle_led(mut led: Output<'static>, signal: &'static Signal<ThreadMode
     }
 }
 
+pub fn setup_trace() {
+    // clear reset for IO port E
+    embassy_stm32::pac::RCC.ahb1rstr().write(|w| w.set_gpioerst(false));
+    // switch trace pins to alternate function
+    embassy_stm32::pac::GPIOE.moder().modify(|w| w.set_moder(2, embassy_stm32::pac::gpio::vals::Moder::ALTERNATE));
+    embassy_stm32::pac::GPIOE.moder().modify(|w| w.set_moder(3, embassy_stm32::pac::gpio::vals::Moder::ALTERNATE));
+    embassy_stm32::pac::GPIOE.moder().modify(|w| w.set_moder(4, embassy_stm32::pac::gpio::vals::Moder::ALTERNATE));
+    embassy_stm32::pac::GPIOE.moder().modify(|w| w.set_moder(5, embassy_stm32::pac::gpio::vals::Moder::ALTERNATE));
+    embassy_stm32::pac::GPIOE.moder().modify(|w| w.set_moder(6, embassy_stm32::pac::gpio::vals::Moder::ALTERNATE));
+    // set trace pins to very high speed
+    embassy_stm32::pac::GPIOE.ospeedr().modify(|w| w.set_ospeedr(2, embassy_stm32::pac::gpio::vals::Ospeedr::VERY_HIGH_SPEED));
+    embassy_stm32::pac::GPIOE.ospeedr().modify(|w| w.set_ospeedr(3, embassy_stm32::pac::gpio::vals::Ospeedr::VERY_HIGH_SPEED));
+    embassy_stm32::pac::GPIOE.ospeedr().modify(|w| w.set_ospeedr(4, embassy_stm32::pac::gpio::vals::Ospeedr::VERY_HIGH_SPEED));
+    embassy_stm32::pac::GPIOE.ospeedr().modify(|w| w.set_ospeedr(5, embassy_stm32::pac::gpio::vals::Ospeedr::VERY_HIGH_SPEED));
+    embassy_stm32::pac::GPIOE.ospeedr().modify(|w| w.set_ospeedr(6, embassy_stm32::pac::gpio::vals::Ospeedr::VERY_HIGH_SPEED));
+    // set alternate function for trace pins
+    embassy_stm32::pac::GPIOE.afr(0).modify(|w| w.set_afr(2, 0));
+    embassy_stm32::pac::GPIOE.afr(0).modify(|w| w.set_afr(3, 0));
+    embassy_stm32::pac::GPIOE.afr(0).modify(|w| w.set_afr(4, 0));
+    embassy_stm32::pac::GPIOE.afr(0).modify(|w| w.set_afr(5, 0));
+    embassy_stm32::pac::GPIOE.afr(0).modify(|w| w.set_afr(6, 0));
+
+    //FIXME: program will halt when following lines are compiled *shrug*
+
+    // // enable clock for IO port E
+    // embassy_stm32::pac::RCC.ahb1enr().write(|w| w.set_gpioeen(true));
+    // // set trace mode to 4 pin synchronous trace
+    // embassy_stm32::pac::DBGMCU.cr().write(|w| w.set_trace_mode(3));
+    // // enable clock
+    // embassy_stm32::pac::DBGMCU.cr().write(|w| w.set_trace_ioen(true));
+}
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
-    embassy_stm32::pac::GPIOE.afr(0).write(|w| w.set_afr(2, 0));
-    embassy_stm32::pac::GPIOE.afr(0).write(|w| w.set_afr(3, 0));
-    embassy_stm32::pac::GPIOE.afr(0).write(|w| w.set_afr(4, 0));
-    embassy_stm32::pac::GPIOE.afr(0).write(|w| w.set_afr(5, 0));
-    embassy_stm32::pac::GPIOE.afr(0).write(|w| w.set_afr(6, 0));
-    embassy_stm32::pac::GPIOE.moder().write(|w| w.set_moder(2, embassy_stm32::pac::gpio::vals::Moder::ALTERNATE));
-    embassy_stm32::pac::GPIOE.moder().write(|w| w.set_moder(3, embassy_stm32::pac::gpio::vals::Moder::ALTERNATE));
-    embassy_stm32::pac::GPIOE.moder().write(|w| w.set_moder(4, embassy_stm32::pac::gpio::vals::Moder::ALTERNATE));
-    embassy_stm32::pac::GPIOE.moder().write(|w| w.set_moder(5, embassy_stm32::pac::gpio::vals::Moder::ALTERNATE));
-    embassy_stm32::pac::GPIOE.moder().write(|w| w.set_moder(6, embassy_stm32::pac::gpio::vals::Moder::ALTERNATE));
-
-    info!("Hello World!");
+    setup_trace();
 
     let led1 = Output::new(p.PA8, Level::High, Speed::Low);
     let led2 = Output::new(p.PA9, Level::High, Speed::Low);
